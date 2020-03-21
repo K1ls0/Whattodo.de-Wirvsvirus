6a5255769a1bc768f02184234e5e6fee7ceb9909
Lor3nzL1ke <lorenz.resch@gmx.net>
2020-03-21 13:23:35 +0100

    worked on button.js

--- web/src/index.js
+++ web/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import "./stylesheets/styles-main.css";
 import {Header} from "./header.js";
 import {Button} from "./button";
+import {LangComponent} from "./language";
 
-const Source = (props) => {
-    return <div>
-        <Button className={"button"} >SPRACHE</Button>
-        <Header className={"header"} ></Header>
-    </div>;
-};
+class Source extends LangComponent {
+    constructor(props) {
+        super(props);
+        this.state = {lang: "DE"};
+    }
+
+    render() {
+        return <div>
+            <Button className={"button"} lang={this.state.lang} ></Button>
+            <Header className={"header"} lang={this.state.lang} ></Header>
+        </div>;
+    }
+}
 
 ReactDOM.render(<Source lang={"ENG"} />, document.getElementById("root"));
